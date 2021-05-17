504ff3016a7bf22041dac1f74b843da76e620499
Henrique Morato <riquemorato@gmail.com>
2021-05-17 17:19:54 -0300

    WIP validação de um curso

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ApplicationRecord
+  validates :name, :price, :code, presence: { message: 'não pode ficar em branco' }
+  validates :code, uniqueness: { message: 'já está em uso' }
 end
+
