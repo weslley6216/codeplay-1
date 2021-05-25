c8486996b7f5901f551a357e5e0d696894f8e716
Henrique Morato <riquemorato@gmail.com>
2021-05-25 17:18:18 -0300

    Unit tests para instructor

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,6 +1,8 @@
 class Instructor < ApplicationRecord
   has_many :courses, dependent: :destroy
 
+  validates :name, :email, presence: true
+
   def display_name
     "#{name} - #{email}"
   end
--- spec/models/instructor_spec.rb
+++ spec/models/instructor_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe Instructor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Instructor do
+  it { should have_many(:courses) }
+  it { should validate_presence_of(:name).with_message('não pode ficar em branco') }
+  it { should validate_presence_of(:email).with_message('não pode ficar em branco') }
+
+  context 'display_name' do
+    it 'should display name and email with hyphen' do
+      instructor = Instructor.new(name: 'Fulano',
+                                  email: 'fulano@email.com')
+      expect(instructor.display_name).to eq('Fulano - fulano@email.com')
+    end
+
+    it 'should display name and email with multiple names' do
+      instructor = Instructor.new(name: 'Fulano Sicrano',
+                                  email: 'fulano@email.com')
+      expect(instructor.display_name).to eq('Fulano Sicrano - fulano@email.com')
+    end
+
+    it 'should display even with empty values' do
+      instructor = Instructor.new(name: '',
+                                  email: 'fulano@email.com')
+      expect(instructor.display_name).to eq(' - fulano@email.com')
+    end
+  end
 end
